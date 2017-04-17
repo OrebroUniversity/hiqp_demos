edf4826b9200db800e740f0fdcb2ae853df3df1e
Yumi <yumi@oru>
2017-04-17 14:43:07 +0200

    Correct cmakelists error

--- scripts/frame_frame_pose.sh
+++ scripts/frame_frame_pose.sh
@@ -1,19 +1,19 @@
 #Add a frame geometric primitive to the gripper which is parametrized by the x/y/z coordinates of the origin and either x/y/z euler angles or the w/x/y/z quaternion describing the orienation relative to 'frame_id'
 rosservice call /yumi/hiqp_joint_velocity_controller/set_primitives "primitives: 
   -
-    name: 'test_frame'
+    name: 'gripper_r_frame'
     type: 'frame'
-    frame_id: 'gripper_r_base'
+    frame_id: 'yumi_link_tool_r'
     visible: true
     color: [0.0, 0.0, 1.0, 1.0]   
-    parameters: [0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
+    parameters: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
   -
     name: 'target_frame'
     type: 'frame'
     frame_id: 'world'
     visible: true
     color: [0.0, 0.0, 1.0, 1.0]   
-    parameters: [0.298, -0.051, 0.131, 1.74, 1.74, 0.0]" 
+    parameters: [0.45, -0.250, 0.170, 0.0, 0.0, 0.0]" 
 
 #The alignment task will orient the frames in the same way
 #The projection task will co-locate the frame origins
@@ -25,14 +25,14 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks "tasks:
     visible: 1
     active: 1
     monitored: 1
-    def_params: ['TDefGeomAlign', 'frame', 'frame', 'test_frame = target_frame', '0']
-    dyn_params: ['TDynLinear', '1.0']
+    def_params: ['TDefGeomAlign', 'frame', 'frame', 'gripper_r_frame = target_frame', '0']
+    dyn_params: ['TDynLinear', '0.7']
   -    
     name: 'frame_frame_projection'
     priority: 2
     visible: 1
     active: 1
     monitored: 1
-    def_params: ['TDefGeomProj', 'frame', 'frame', 'test_frame = target_frame']
-    dyn_params: ['TDynLinear', '1.0']"
+    def_params: ['TDefGeomProj', 'frame', 'frame', 'gripper_r_frame = target_frame']
+    dyn_params: ['TDynLinear', '0.7']"
 
