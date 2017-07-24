e0ca7f5e45e243c453e5f0c9556b731cfb661fa4
Robert Krug <robert.krug@oru.se>
2017-07-24 14:14:52 +0200

    reverted to: 6257c93d8a870c74f50699537b5d2dd9d53284e5 in order to maintain compatibility with the current hiqp master

--- scripts/joint_configuration.sh
+++ scripts/joint_configuration.sh
@@ -1,10 +1,9 @@
 #Task makes the joint angles converge with linear dynamics (gain -1.0) to the specified values - joint order follows the one in /config/yumi.yaml 
-rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
-"tasks:  
-- name: 'joint_configuration'
-  priority: 2
-  visible: 1
-  active: 1
-  monitored: 1
-  def_params: ['TDefFullPose', '-0.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '0.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
-  dyn_params: ['TDynLinear', '1.0'] "
+rosservice call /yumi/hiqp_joint_velocity_controller/set_task \
+"name: 'joint_configuration'
+priority: 2
+visible: 1
+active: 1
+monitored: 1
+def_params: ['TDefFullPose', '-0.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '1.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
+dyn_params: ['TDynLinear', '1.0']"
