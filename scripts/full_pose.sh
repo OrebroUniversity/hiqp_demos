f157cbfa0d3153cd27822f6270c771193a000895
Robert Krug <robert.krug@oru.se>
2017-09-01 14:48:34 +0200

    and more shellscripts

---
+++ scripts/full_pose.sh
@@ -0,0 +1,10 @@
+rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
+"tasks:
+- name: 'full_pose'
+  priority: 2
+  visible: 1
+  active: 1
+  monitored: 1
+  def_params: ['TDefFullPose', '-0.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '0.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
+  dyn_params: ['TDynPD', '1.0', '3.0'] "
+
--- scripts/joint_configuration.sh
+++ scripts/joint_configuration.sh
@@ -1,10 +1,10 @@
-#Task makes the joint angles converge with linear dynamics (gain -1.0) to the specified values - joint order follows the one in /config/yumi.yaml 
 rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
-"tasks:  
-- name: 'joint_configuration'
+"tasks: 
+- name: 'yumi_link_7_r_jntconfig'
   priority: 2
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefFullPose', '-0.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '0.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
-  dyn_params: ['TDynPD', '2.0', '3.0'] "
+  def_params: ['TDefJntConfig', 'yumi_link_7_r', '-0.5']
+  dyn_params: ['TDynPD', '0.5', '2.0']"
+
