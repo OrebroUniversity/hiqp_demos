d8aff773a93334d5b18ae82e42664b7a946ff146
Robert Krug <robert.krug@oru.se>
2017-09-01 18:50:28 +0200

    played with the robot

--- scripts/joint_configuration.sh
+++ scripts/joint_configuration.sh
@@ -1,10 +1,10 @@
 rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
 "tasks: 
-- name: 'yumi_link_7_r_jntconfig'
+- name: 'yumi_link_6_r_jntconfig'
   priority: 2
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefJntConfig', 'yumi_link_7_r', '-0.5']
-  dyn_params: ['TDynPD', '0.5', '2.0']"
+  def_params: ['TDefJntConfig', 'yumi_link_1_r', '1.0']
+  dyn_params: ['TDynPD', '1.5', '2.5']"
 
