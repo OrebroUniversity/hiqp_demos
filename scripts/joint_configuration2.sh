ef316bda3b3ca808c52fec685dfa1a280fd0b497
tsv <todor.stoyanov@oru.se>
2017-09-05 10:49:46 +0200

    preparing for merge

--- scripts/joint_configuration2.sh
+++ scripts/joint_configuration2.sh
@@ -6,5 +6,5 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefFullPose', '-0.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '0.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
+  def_params: ['TDefFullPose', '-1.42', '-1.48', '1.21', '0.75', '-0.8', '0.45', '0.21', '0.42', '-1.48', '-1.21', '0.75', '0.8', '0.45', '1.21']
   dyn_params: ['TDynLinear', '0.7'] "
