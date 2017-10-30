f474ca3e693c39320eed4a5493623f8c49219e98
hkaraoguz <hkaraoguz@yahoo.com.tr>
2017-10-30 16:40:31 +0100

    added yumi_setup param in launch files

--- scripts/joint_configuration.sh
+++ scripts/joint_configuration.sh
@@ -1,3 +1,4 @@
+
 rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
 "tasks: 
 - name: 'yumi_jntconfig'
@@ -5,6 +6,6 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefJntConfig', 'yumi_link_7_r', '1.0']
-  dyn_params: ['TDynPD', '0.05', '0.5']"
+  def_params: ['TDefJntConfig', 'yumi_link_7_r', '-1.0']
+  dyn_params: ['TDynPD', '1.0', '2.0']"
 
