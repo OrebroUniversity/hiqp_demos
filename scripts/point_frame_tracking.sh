232ca38370799e6303ce0dd09414be7b91e74568
FACT PC <hkaraoguz@yahoo.com.tr>
2017-12-15 09:44:33 +0100

    shellscript tweak

--- scripts/point_frame_tracking.sh
+++ scripts/point_frame_tracking.sh
@@ -5,7 +5,7 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_primitives \
   frame_id: 'gripper_r_base'
   visible: true
   color: [1.0, 0.0, 0.0, 1.0]   
-  parameters: [0.0, 0.0, 0.15]
+  parameters: [0.0, 0.0, 0.0]
 - name: 'target_frame'
   type: 'frame'
   frame_id: 'yumi_base_link'
@@ -20,8 +20,8 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_tasks \
   visible: 1
   active: 1
   monitored: 1
-  def_params: ['TDefTracking', 'point', 'frame', 'ee_point = target_frame', '0.04']
-  dyn_params: ['TDynPD', '20.0', '10.0'] 
+  def_params: ['TDefTracking', 'point', 'frame', 'ee_point = target_frame', '0.05']
+  dyn_params: ['TDynPD', '2.0', '4.0'] 
 - name: 'full_pose'
   priority: 3
   visible: 1
